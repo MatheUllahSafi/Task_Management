8ede85941bcdec5eb610a3f03bdc6109320b8400
MatheUllah <matheullahsafi@gmail.com>
2025-03-16 01:16:12 +0500

    added host

--- task_management/settings.py
+++ task_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)0z56vm8gohdcimw=0(#p9bkp=%7vwsc#)wem9+i^(o8!cha1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matheullah.pythonanywhere.com']
 
 
 # Application definition
